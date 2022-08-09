1bd76e45c61a7145b9ee2a3ce5f9fd95212f9f50
“satoshyyy <pancakepancakekuitai@gmail.com>
2022-08-09 19:56:59 +0900

    Post モデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   # ***** 以下を追加 *****
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 500 }
+
   # ***** 以上を追加 *****
 end
