05fc37ee4b57b9be68ec620b28f16de0479e98bc
“satoshyyy <pancakepancakekuitai@gmail.com>
2022-08-09 23:42:05 +0900

    バリデーションエラー時に投稿ページをレンダリングするように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,17 +13,25 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create!(post_params)
-    # ***** 次を編集 *****
-    redirect_to post, notice: "投稿しました"
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    @post.update!(post_params)
-    redirect_to @post, notice: "更新しました"
+    if @post.update(post_params)
+      redirect_to @post, notice: "更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
